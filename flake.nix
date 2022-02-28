e251975b71886841e3dd83860eb351693f684396
Steven Shaw <steven@steshaw.org>
2022-02-28 13:36:53 +1000

    Use nixos-21.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A simple Go package";
 
   # Nixpkgs / NixOS version to use.
-  inputs.nixpkgs.url = "nixpkgs/nixos-21.11";
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }:
     let
